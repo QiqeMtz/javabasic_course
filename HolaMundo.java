130f06213c697f39781651e7c40efe97ecd7ea96
Luis Enrique Jeronimo Martinez <qiqemtz@Luiss-MacBook-Pro.local>
2017-08-06 23:25:32 -0500

    Little examples

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class HolaMundo {
     public static void main(String[] args){
         System.out.print("Hola Mundo\n");
+        int [][] conjunto = new int[3][4];
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<4; j++){
+                conjunto[i][j] = 1;
+            }
+        }
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<4; j++){
+                System.out.print(conjunto[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
